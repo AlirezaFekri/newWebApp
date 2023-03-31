baa2d54127bc82facf13b41032a503b974439b34
Alirza Fekri <alirezafekri2003@gmail.com>
2023-03-31 04:39:21 +0430

    Design part of filter tab

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,34 +3,36 @@ module.exports = {
   content: ["./public/**/*.html"],
   theme: {
     extend: {
-      fontFamily:{
-        'sans':['Yekan']
+      fontFamily: {
+        'sans': ['Yekan']
       },
-      colors:{
+      colors: {
         'Orange-700': '#DE3618',
         'Orange-600': '#FC5858',
         'Orange-500': '#F25E57',
         'Orange-400': '#FF755C',
         'Orange-300': '#FF9F8E',
         'Orange-100': '#FFCBCB',
-        'Slate-900' : '#161D25',
-        'Slate-800' : '#222F5D',
-        'Slate-700' : '#606060',
-        'Slate-200' : '#E2E2E2',
-        'Stone-100' : '#F4F4F4',
-        'Stone-50' : '#FDFDFD',
-        'Gray-300' : '#AFAFAF',
-        'Gray-200' : '#EBEBEB',
+        'Slate-900': '#161D25',
+        'Slate-800': '#222F5D',
+        'Slate-700': '#606060',
+        'Slate-200': '#E2E2E2',
+        'Stone-100': '#F4F4F4',
+        'Stone-50': '#FDFDFD',
+        'Gray-300': '#AFAFAF',
+        'Gray-200': '#EBEBEB',
         'Indigo-600': '#8750FB',
         'Yellow-400': '#FFDE74'
       }
     },
-    
-    dropShadow:{
-      '4xl':'0px 4px 4px rgba(0, 0, 0, 0.25)',
-      '5xl':'0px 3.09px 6.18px rgba(0, 0, 0, 0.08)'
+
+    dropShadow: {
+      '4xl': '0px 4px 4px rgba(0, 0, 0, 0.25)',
+      '5xl': '0px 3.09px 6.18px rgba(0, 0, 0, 0.08)',
+      '6xl': ' 0px -4px 8px 0px rgba(0, 0, 0, 0.08)'
+
     },
-    backgroundImage:{
+    backgroundImage: {
       'logo': "url('/assets/img/Logo.svg')",
     }
   },
